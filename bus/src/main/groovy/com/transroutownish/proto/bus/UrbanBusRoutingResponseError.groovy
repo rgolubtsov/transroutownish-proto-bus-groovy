67fe980dbabb6b5570485f5c20794fa65f09e493
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2023-10-05 23:30:30 +0300

    Bump version number to 0.1.9.

--- bus/src/main/groovy/com/transroutownish/proto/bus/UrbanBusRoutingResponseError.groovy
+++ bus/src/main/groovy/com/transroutownish/proto/bus/UrbanBusRoutingResponseError.groovy
@@ -2,7 +2,7 @@
  * bus/src/main/groovy/com/transroutownish/proto/bus/
  * UrbanBusRoutingResponseError.groovy
  * ============================================================================
- * Urban bus routing microservice prototype (Groovy port). Version 0.1.5
+ * Urban bus routing microservice prototype (Groovy port). Version 0.1.9
  * ============================================================================
  * A daemon written in Groovy, designed and intended to be run
  * as a microservice, implementing a simple urban bus routing prototype.
@@ -19,7 +19,7 @@ package com.transroutownish.proto.bus
  * when one of the <strong>4xx Client Error</strong> section's errors occurred
  * during processing the request.
  *
- * @version 0.1.5
+ * @version 0.1.9
  * @since   0.1.5
  */
 class UrbanBusRoutingResponseError {
